150ce98d94536f33c0a0582d6f4f94087221a8db
Gomnonix <lee20481997@gmail.com>
2024-06-03 00:11:16 +0900

    feat:  REST framework를 이용해 데이터를 요청, 생성, 수정 등의 기능을 구현

---
+++ membership_custom/account/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser 
+from common.models import CommonModel
+
+class Account(AbstractUser, CommonModel):
+    phone_number = models.CharField(max_length=20)
+
+# class Account(models.Model): # Model을 상속받는다
+# 	name = models.CharField(max_length=20) # 짧은 문장
+# 	email = models.CharField(max_length=20) # 긴 텍스트 문장
+# 	phone_number = models.CharField(max_length=20)
+
+# 	def __str__(self):
+# 		return self.name
---
+++ membership_custom/address/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+# urlpatterns = [
+#     path("", views.Addresses.as_view()), 
+# 	path("<int:address_id>/address", views.Addresses.as_view())
+# ]
+
+urlpatterns = [
+path('', views.AddressList.as_view(), name='address-list'),
+path('/<int:pk>/', views.AddressDetail.as_view(), name='address-detail'),
+path('/<int:user_id>/add', views.CreateUserAddress.as_view(), name='create-user-address'),
+path('/<int:pk>/update', views.UpdateAddress.as_view(), name='update-address'),
+path('/<int:pk>/delete', views.DeleteAddress.as_view(), name='delete-address'),
+]
